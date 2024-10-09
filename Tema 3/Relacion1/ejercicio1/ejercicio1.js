e5baafaff30d9b76a0dbaa8ec0ff0d3010ad8d18
alxrgdriguez <sralexred@gmail.com>
2024-10-09 17:15:19 +0200

    ejercicio1

---
+++ Tema 3/Relacion1/ejercicio1/ejercicio1.js
@@ -0,0 +1,55 @@
+// Funcion para saber el dia del Semana
+
+function diaSemana(numero) {
+
+
+    switch (numero) {
+
+        case 0:
+
+            alert("Hoy es Domingo");
+            break;
+
+        case 1:
+
+            alert("Hoy es Lunes");
+            break;
+
+        case 2:
+
+            alert("Hoy es Martes");
+            break;
+
+        case 3:
+
+            alert("Hoy es Miercoles");
+            break;
+
+        case 4:
+
+            alert("Hoy es Jueves");
+            break;
+
+        case 5:
+
+            alert("Hoy es Viernes");
+            break;
+
+        case 6:
+
+            alert("Hoy es Sabado");
+            break;
+
+        default:
+            alert("Número no válido. Por favor, introduce un número entre 0 y 6.");
+            break;
+
+    }
+
+
+
+}
+
+let diasemana = parseInt(prompt("Introduce un numero entre 0 y 6, te diré que dia de la semana es: "));
+
+diaSemana(diasemana);
